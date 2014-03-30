2a7c4257dada49fe1af4a9d49b5c6b9b39078adf
Amedee d'Aboville <amedee.daboville@gmail.com>
2014-03-29 23:55:56 -0400

    Added baddies, they move randomly

--- js/autopoetic.js
+++ js/autopoetic.js
@@ -4,6 +4,8 @@ $(document).mousemove(function(e){
 
 var FPS = 30;
 size = 100;
+numBaddies = 0;
+baddies = [];
 setInterval(function() {
       update();
         draw();
@@ -11,8 +13,41 @@ setInterval(function() {
 
 function update() {
     size -= .1;
+    if( Math.random() < 0.1) {
+        addBaddie();
+    }
+    for (var i = 0; i < baddies.length; i++) 
+    {
+        var b = baddies[i];
+        console.log(b);
+        b.x += b.speedx;
+        b.y += b.speedy;
+    }
 }
 function draw() {
     $("#cell").width(size);
     $("#cell").height(size);
+    for (var i = 0; i < baddies.length; i++) 
+    {
+        var bad = baddies[i];
+        $('#baddie' + i ).offset({ top: bad.x, left: bad.y})
+    }
+
+}
+
+function addBaddie() {
+    numBaddies++;
+    baddies.push(new Baddie());
+    var baddie = baddies[baddies.length];
+    var bad = $('body').append('<img id="baddie' + numBaddies + '" src="img/cell.png"></img>');
+    bad.width(bad.size);
+    bad.height(bad.size);
+}
+
+function Baddie() {
+    this.size = Math.random() * 30;
+    this.speedx = Math.random() * 20;
+    this.speedy = Math.random() * 20;
+    this.x = Math.random() * $(window).width();
+    this.y = Math.random() * $(window).height();
 }
