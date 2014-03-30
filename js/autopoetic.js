33791e5103eb09d7d2c1028ec626f186fa8c05c0
Amedee d'Aboville <amedee.daboville@gmail.com>
2014-03-29 23:24:58 -0400

    Got blob to get smaller over time

--- js/autopoetic.js
+++ js/autopoetic.js
@@ -1,3 +1,18 @@
 $(document).mousemove(function(e){
     $("#cell").css({left:e.pageX, top:e.pageY});
 });
+
+var FPS = 30;
+size = 100;
+setInterval(function() {
+      update();
+        draw();
+}, 1000/FPS);
+
+function update() {
+    size -= .1;
+}
+function draw() {
+    $("#cell").width(size);
+    $("#cell").height(size);
+}
